3490ad7ab19c03c8294954fe9df00d8da7166063
revanthsaich <revanthsaichaparala05@gmail.com>
2025-01-31 22:23:53 +0530

    added auth

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './Home.css';  // Import the CSS file for Home
 import Slider from '../Slider/Slider.jsx';
+ import { useEffect } from'react';
 import FeaturedProducts from '../FeaturedProducts/FeaturedProducts.jsx';
-
+import { useNavigate } from 'react-router-dom';
 const Home = () => {
-  
+  const navigate = useNavigate();  
+  const token = localStorage.getItem("authToken"); 
+  useEffect(() => {
+    if (!token) {
+      navigate("/signin"); 
+    }
+  }, [token, navigate]);
+
+  if (!token) {
+    return null;  
+  }
   return (
     <div className="home-container">
       <Slider />
